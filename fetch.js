841bafc637d3d5c3e912e9ec0b167d640bca9cfd
shawonvai25 <abdullahalshawon25@gmail.com>
2024-12-26 02:09:39 +0600

    fetch.js added

--- fetch.js
+++ fetch.js
@@ -1,1 +1,16 @@
-console.log('fetching');
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//       .then(response => response.json())
+//       .then(json => console.log(json))
+
+
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
+
+// fetch(url)
+// .then(response => response.json())
+// .then(json => console.log(json))
+
+function loadData(){
+      fetch(url)
+      .then(response => response.json())
+      .then(json => console.log(json))
+}
