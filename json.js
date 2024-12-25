d472932610f75ca283b43a7fb3542ccf000c99bc
shawonvai25 <abdullahalshawon25@gmail.com>
2024-12-26 01:26:09 +0600

    json.js added

--- json.js
+++ json.js
@@ -1,26 +1,31 @@
-const user = { id:1, name:"Gorib Amir", job:'actor'};
-// JavaScript Object Notation (JSON)
-const stringified = JSON.stringify(user);
-// console.log(user);
-// console.log(stringified);
+ const user = {id:1, name:'Gorib Amir', job:'actor'};
+//  JavaScript Object Notation (JSON)
+ const stringified = JSON.stringify(user);
+//  console.log(user);
+//  console.log(stringified);
 
-/*  
-{ id: 1, name: 'Gorib Amir', job: 'actor' }
+ /* 
+ { id: 1, name: 'Gorib Amir', job: 'actor' }
 {"id":1,"name":"Gorib Amir","job":"actor"}
-*/
+ */
+
 
 const shop = {
     owner:'Alia',
-    address: {
-        street:'kochukhet voot er goli',
-        city:'Dhk',
+    address:{
+        Street:'kochukhet voot er goli',
+        city:'Dhaka',
         country:'Bangladesh'
     },
-    products:['laptop','mic', 'monitor','keyboard'],
+    products:['Laptop','mic','monitor','keyboard'],
     revenue:45000,
     isOpen: true,
-    isNew: false
+    isNew:false
 };
-console.log(shop); 
+
+console.log(shop);
 const shopJSON = JSON.stringify(shop);
 console.log(shopJSON);
+
+const shopObj = JSON.parse(shopJSON); 
+console.log(shopObj);   
